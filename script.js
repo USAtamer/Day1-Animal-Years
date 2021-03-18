da71f0700486657df20ea714396129682920653f
Tamer Tasci <ttasci@HSIELPASO.ORG>
2021-03-18 08:43:45 -0600

    create index-style-script files

---
+++ script.js
@@ -0,0 +1,56 @@
+const myAge = 25; // my current age
+
+console.log(myAge);
+
+let earlyYears = 2; // first 2 years of the dog
+
+console.log(earlyYears);
+
+ earlyYears = earlyYears * 10.5;
+
+console.log(earlyYears);
+
+let laterYears  = myAge - 2; 
+
+console.log(laterYears);
+
+laterYears *= 4; // my lateryears in dog years   
+
+console.log(laterYears);
+
+let myAgeInDogyears = earlyYears + laterYears;
+
+console.log(myAgeInDogyears);
+
+let myName ='Tamer'.toLowerCase(); 
+
+console.log(myName);
+
+console.log('', myName , ' I am' ,myAge ,'years old in human years which is' ,myAgeInDogyears,'years old in dog years');
+
+console.log( `My name is ${myName}. I am 
+${myAge} years old in human years which is 
+${myAgeInDogyears} years old in dog years.
+`)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
